7da28a079c5c211a7ba8481ce9225469dc3d54f5
Carson McKinstry <carson.mckinstry@trivago.com>
2022-02-24 16:10:31 +0100

    Starting over for real

--- src/components/GameBoard/Round/Round.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-export const Round: FC = ({ children }) => {
-    return (
-        <section className="flex flex-col items-center justify-between h-full w-full">
-            {children}
-        </section>
-    );
-};
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -2,8 +2,6 @@ import { NextPage } from "next";
 import Link from "next/link";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
-import { ProgressBar } from "@components/GameBoard/ProgressBar";
-import { AlbumArtwork } from "@components/GameBoard/AlbumArtwork";
 
 const getMe = gql`
     query Me {
@@ -22,12 +20,6 @@ const GameHome: NextPage = () => {
             <Link href="/api/logout">Logout</Link>
             {loading && <p>Loading...</p>}
             {!loading && <p>Hello, {data.me.display_name}</p>}
-            <Link href="/game/new">New Game</Link>
-            <ProgressBar playing />
-            <AlbumArtwork
-                playing
-                url="https://source.unsplash.com/random/300×300"
-            />
         </div>
     );
 };
