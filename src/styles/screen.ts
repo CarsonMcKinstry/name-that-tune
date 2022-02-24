ee3a09fa05391b86bf3adfbc7d9b6b8a6a4db400
Carson McKinstry <carson.mckinstry@gmail.com>
2022-02-24 17:22:36 +0100

    remove styled components

--- src/styles/screen.ts
+++
@@ -1,9 +0,0 @@
-import { Screen } from "./styled";
-
-export const screen: Screen = {
-    tablet: "640px",
-    laptop: "768px",
-    desktop: "1024px",
-    wide: "1280px",
-    ultrawide: "1536px",
-};
